4afb17adeea7f589865c4a219ab7be345f3501f6
David Haber-Zelanto <david.haber.zelanto@gmail.com>
2022-02-17 23:32:31 +0100

    Final updates

--- roller_coasters/script.py
+++ roller_coasters/script.py
@@ -1,6 +1,7 @@
 # %%
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 
 # load rankings data:
@@ -16,6 +17,7 @@ def plot_rankings(coaster_name,df,park_name):
     rankings = df[['Rank','Year of Rank']][(df.Name == coaster_name) & (df.Park == park_name)]
     
     # create plot
+    plt.figure()
     ax = plt.subplot()
     plt.plot(rankings['Year of Rank'], rankings.Rank)
     ax.invert_yaxis()
@@ -24,14 +26,12 @@ def plot_rankings(coaster_name,df,park_name):
     plt.xlabel('Year')
     plt.ylabel('Rank')
     plt.title(coaster_name + ' Rankings')
-
     plt.show(block = False)
 
 # test function
 plot_rankings('El Toro',coasters_wood,'Six Flags Great Adventure')
-plt.clf()
 plot_rankings('El Toro',coasters_wood,'Freitzeitpark Plohn')
-plt.clf()
+
 
 # %%
 # write function to plot rankings over time for 2 roller coasters here:
@@ -41,6 +41,7 @@ def plot_2rankings(coaster1,coaster2,df1,df2,park_name1,park_name2):
     rankings2 = df2[['Rank','Year of Rank']][(df2.Name == coaster2) & (df2.Park == park_name2)]
     
     # create plot
+    plt.figure()
     ax = plt.subplot()
     plt.plot(rankings1['Year of Rank'], rankings1.Rank,label = coaster1)
     plt.plot(rankings2['Year of Rank'], rankings2.Rank, label = coaster2)
@@ -53,8 +54,7 @@ def plot_2rankings(coaster1,coaster2,df1,df2,park_name1,park_name2):
 
 # test function
 plot_2rankings('El Toro','Boulder Dash',coasters_wood,coasters_wood,'Six Flags Great Adventure','Lake Compounce')
-plt.clf()
-plt.close()
+
 
 # %%
 # write function to plot top n rankings over time here:
@@ -75,7 +75,7 @@ def plot_nrankings(n,df):
 
 # test function
 plot_nrankings(5,coasters_wood)
-plt.clf()
+
 
 # %%
 # load roller coaster data here:
@@ -88,8 +88,10 @@ def plot_histogram(df, column):
     # remove outliers for height
     if column == 'height':
         # plt.hist(df[column])
+        plt.figure()
         plt.hist(df[column][df[column] <= 140].dropna())  
     else:
+        plt.figure()
         plt.hist(df[column].dropna())
 
     # rename
@@ -103,13 +105,9 @@ def plot_histogram(df, column):
 
 # test function
 plot_histogram(coasters_df, 'speed')
-plt.clf()
 plot_histogram(coasters_df, 'length')
-plt.clf()
 plot_histogram(coasters_df, 'num_inversions')
-plt.clf()
 plot_histogram(coasters_df, 'height')
-plt.clf()
 
 
 # %%
@@ -122,6 +120,7 @@ def plot_bar_inversions(df,park_name):
     inversions = park_df.sort_values('num_inversions', ascending = False)['num_inversions'] 
     
     # create bar plot
+    plt.figure()
     plt.bar(range(len(inversions)),inversions)
     ax = plt.subplot()
     ax.set_xticks(range(len(coasters)))
@@ -134,11 +133,8 @@ def plot_bar_inversions(df,park_name):
 
 # test function
 plot_bar_inversions(coasters_df,'Disneyland Park')
-plt.clf()
 plot_bar_inversions(coasters_df,'Walibi Rhône Alpes')
-plt.clf()
 plot_bar_inversions(coasters_df, 'Six Flags Great Adventure')
-plt.clf()
 
 # %%
 # write function to plot pie chart of operating status:
@@ -148,6 +144,7 @@ def plot_pie_operating(df):
     noperating = sum(df.status == 'status.closed.definitely')
 
     # create pie chart
+    plt.figure()
     plt.pie([operating, noperating],autopct = '%0.1f%%',labels = ['Operational','Closed'])
     plt.axis('equal')
     plt.title('Operational coasters')
@@ -155,8 +152,6 @@ def plot_pie_operating(df):
     
 # test function
 plot_pie_operating(coasters_df)
-
-plt.clf()
 
 # %%
 # write function to create scatter plot of any two numeric columns:
@@ -167,6 +162,7 @@ def plot_scatter(df, column1, column2):
         df = df[df['height'] <= 140]
     
     # create scatter plot
+    plt.figure()
     plt.scatter(df[column1],df[column2])
     plt.xlabel(column1.capitalize())
     plt.ylabel(column2.capitalize())
@@ -175,26 +171,24 @@ def plot_scatter(df, column1, column2):
 
 # test function
 plot_scatter(coasters_df, 'length', 'height')
-plt.clf()
 plot_scatter(coasters_df, 'length', 'num_inversions')
-plt.clf()
 plot_scatter(coasters_df, 'speed', 'height')
-plt.clf()
+
 
 # %%
 # What roller coaster seating type is most popular?
 # And do different seating types result in 
 # higher/faster/longer roller coasters?
-import seaborn as sns
 
-# print(coasters_df.head())
 
 # extract seating type data
 seatings = coasters_df.seating_type.value_counts()
-# print(seatings)
+
+# Rename to 'Other' seating_types that have a count less than 50
 mask = coasters_df.seating_type.isin(seatings[seatings<50].index)
-coasters_df.seating_type[mask] = 'Other'
-seatings = coasters_df.seating_type.value_counts()
+coasters_df_pie = coasters_df
+coasters_df_pie.seating_type[mask] = 'Other'
+seatings = coasters_df_pie.seating_type.value_counts()
 
 # create a pie chart
 labels = seatings.index
@@ -203,14 +197,14 @@ plt.pie(values,labels = labels, autopct = '%d%%')
 plt.title('Rollercoasters seatting types' )
 plt.show(block = False)
 
-# create a bar chart for seating types 
+# create a box plot for seating types and a choosen feature
 def plot_seatingtype(df,column):
     plt.figure(figsize = (12,7))
     # sns.set_style()
     sns.boxplot(data = df.dropna(), x = 'seating_type', y = column)
     plt.xlabel('Seating type')
     plt.ylabel(column.capitalize())
-    plt.show()
+    plt.show(block = False)
 
 plot_seatingtype(coasters_df,'length')
 plot_seatingtype(coasters_df,'speed')
@@ -220,26 +214,70 @@ plot_seatingtype(coasters_df,'speed')
 # Do roller coaster manufacturers have any specialties
 # (do they focus on speed, height, seating type, or inversions)?
 
-manufacturers = coasters_df.dropna().groupby('manufacturer')[['speed','length','height','num_inversions']].mean().reset_index()
-print(manufacturers)
-# print(manufacturers[manufacturers.manufacturer == 'ACE Coaster Classic'])
-print(manufacturers[['speed','length','height','num_inversions']][manufacturers.manufacturer == 'ACE Coaster Classic'])
+# create a bar chart for a specific feature for top manufacturers
+def plot_manufact_feature(df,column):
+    
+    # select which manufact. are best in this feature
+    manufacturers = df.dropna().groupby('manufacturer')[column].mean().reset_index().sort_values(column, ascending = False)
+    print(manufacturers)
+    top_manufacturers = manufacturers.manufacturer.to_list()[:10]
+    print(type(top_manufacturers))
 
+    # select only those manufact. data
+    mask = df.manufacturer.isin(top_manufacturers)
+    new_df = df[mask]
+    print(mask)
 
-# create a bar chart for seating types 
-def plot_seatingtype(df,manufact):
+    # create a bar plot
     plt.figure(figsize = (12,7))
-    # sns.set_style()
-    plt.bar(range(4),df[['speed','length','height','num_inversions']][df.manufacturer == manufact])
+    sns.set_style('whitegrid')
+    sns.barplot(data = new_df,x = 'manufacturer', y = column)
+    plt.xlabel('Manufacturer')
+    plt.ylabel(column.capitalize())
+    plt.title(column.capitalize() + ' for top 10 manufacturers')
+    plt.xticks(rotation = 90)
 
-    plt.xlabel('Seating type')
-    # plt.ylabel(column.capitalize())
-    plt.show()
-    print(df[manufact])
+    plt.show(block = False)
 
-plot_seatingtype(manufacturers,'ACE Coaster Classic')
+plot_manufact_feature(coasters_df,'speed')
+# plot_manufact_feature(coasters_df,'height')
+# plot_manufact_feature(coasters_df,'length')
+plot_manufact_feature(coasters_df,'num_inversions')
 
+# compare seating_types for a specific manufacturer
+def plot_manufacturer_seating_type(df, manufacturer):
+    new_df = df[df.manufacturer == manufacturer]
+    sns.countplot(data = new_df, x = 'seating_type')
+    plt.title('Seating types for '+ manufacturer)
+    plt.show(block = False)
 
+plot_manufacturer_seating_type(coasters_df, 'Giovanola')
 # %%
-
 # Do amusement parks have any specialties?
+
+# create a bar chart for a specific feature for the amusement parks
+def plot_park_feature(df,column):
+    
+    # select which parks are best in this feature
+    parks = df.dropna().groupby('park')[column].mean().reset_index().sort_values(column, ascending = False)
+    print(parks)
+    top_parks = parks.park.to_list()[:10]
+    print(type(top_parks))
+
+    # select only those park data
+    mask = df.park.isin(top_parks)
+    new_df = df[mask]
+    print(mask)
+
+    # create a bar plot
+    plt.figure(figsize = (12,7))
+    sns.set_style('whitegrid')
+    sns.barplot(data = new_df,x = 'park', y = column)
+    plt.xlabel('Park')
+    plt.ylabel(column.capitalize())
+    plt.title(column.capitalize() + ' for top 10 parks')
+    plt.xticks(rotation = 90)
+    plt.show()
+
+plot_park_feature(coasters_df,'speed')
+plot_park_feature(coasters_df,'length')
